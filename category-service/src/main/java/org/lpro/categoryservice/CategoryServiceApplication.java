971e56bb8c7d86b6835a298342dce6085191cedd
Briand-Lucas <lucas.briand88@outlook.fr>
2019-03-10 09:06:27 +0100

    Suppresion .zip

--- category-service/src/main/java/org/lpro/categoryservice/CategoryServiceApplication.java
+++ category-service/src/main/java/org/lpro/categoryservice/CategoryServiceApplication.java
@@ -2,8 +2,10 @@ package org.lpro.categoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("org.lpro.categoryservice")
 public class CategoryServiceApplication {
 
 	public static void main(String[] args) {
